bf5bf4e89d7f5382e52d51abf71b7db128e4f7d0
Fred Drake <fdrake@acm.org>
2004-06-22 15:15:07 +0000

    - move the accesslog configuration out of ZConfig - remove the <hitlog> configuration section; use <accesslog> instead

---
+++ accesslog.py
@@ -0,0 +1,38 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Configuration support for the access log.
+
+This assumes that access logging is being performed through the logger
+object returned by logging.getLogger('accesslog').
+
+$Id$
+"""
+import logging
+
+from ZConfig.components.logger.logger import LoggerFactoryBase
+
+
+class AccessLogFactory(LoggerFactoryBase):
+    """Logger factory that returns the access logger."""
+
+    name = "accesslog"
+
+    def create(self):
+        logger = LoggerFactoryBase.create(self)
+        logger.setLevel(logging.INFO)
+        logger.propagate = False
+        formatter = logging.Formatter()
+        for handler in logger.handlers:
+            handler.setFormatter(formatter)
+        return logger
