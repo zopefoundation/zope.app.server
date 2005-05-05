78dbf48c7ee357ed3bb87be831370ba2e9a75243
Stephan Richter <stephan.richter@gmail.com>
2005-05-05 19:49:32 +0000

    foom changed the web2 API a bit, so let's update our code to this new API. He also merged in my commona access logger code, so we do not have to worry about logging anymore. The only task left is to get to the user info somehow, but foom will provide a hook for me soon.

--- http.py
+++ http.py
@@ -17,6 +17,8 @@ $Id$
 """
 import twisted.web2.wsgi
 import twisted.web2.server
+import twisted.web2.http
+import twisted.web2.log
 
 from zope.app.server.server import ServerType, SSLServerType
 from zope.app import wsgi
@@ -25,8 +27,9 @@ from zope.app import wsgi
 def createHTTPFactory(db):
     resource = twisted.web2.wsgi.WSGIResource(
         wsgi.WSGIPublisherApplication(db))
+    resource = twisted.web2.log.LogWrapperResource(resource)
     
-    return twisted.web2.server.Site(resource)
+    return twisted.web2.http.HTTPFactory(twisted.web2.server.Site(resource))
 
 
 http = ServerType(createHTTPFactory, 8080)
@@ -37,8 +40,9 @@ https = SSLServerType(createHTTPFactory, 8443)
 def createPMHTTPFactory(db):
     resource = twisted.web2.wsgi.WSGIResource(
         wsgi.PMDBWSGIPublisherApplication(db))
+    resource = twisted.web2.log.LogWrapperResource(resource)
     
-    return twisted.web2.server.Site(resource)
+    return twisted.web2.http.HTTPFactory(twisted.web2.server.Site(resource))
 
 pmhttp = ServerType(createPMHTTPFactory, 8080)
 
