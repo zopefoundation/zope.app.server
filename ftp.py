fe4fcd4ed3f54eac14c13c3a40b7d1ddfa0cbf17
Brian Sutherland <brian@vanguardistas.net>
2007-04-09 22:40:27 +0000

    convert zope.app.server to use resource and application factories.

--- ftp.py
+++ ftp.py
@@ -26,8 +26,8 @@ import zope.interface
 class FTPRequestFactory(object):
     """FTP Request factory
 
-    FTP request factories for a given database create FTP requets with
-    publications on the given database:
+    FTP request factories for a given resource factory create FTP requests with
+    publications on the given resource factory:
 
       >>> from zope.app.publication.interfaces import IResourceFactory
       >>> class Stub:
@@ -42,8 +42,8 @@ class FTPRequestFactory(object):
     """
     zope.interface.implements(IPublicationRequestFactory)
 
-    def __init__(self, db):
-        self.publication = FTPPublication(db)
+    def __init__(self, resource_factory):
+        self.publication = FTPPublication(resource_factory)
 
     def __call__(self, input_stream, env):
         request = FTPRequest(input_stream, env)
