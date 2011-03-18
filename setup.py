1c0c6963b29210cb33e8bcd6e1a8e0bf80397ac3
Gediminas Paulauskas <menesis@pov.lt>
2011-03-18 16:02:31 +0000

    Remove test_zpasswd.py, it was moved to zope.password. Bump version to 3.6.0dev

--- setup.py
+++ setup.py
@@ -17,8 +17,6 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 """Setup for zope.app.server package
-
-$Id$
 """
 import os
 from setuptools import setup, find_packages
@@ -27,7 +25,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.server',
-      version = '3.5.1dev',
+      version = '3.6.0dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='ZServer integration for Zope 3 Applications',
