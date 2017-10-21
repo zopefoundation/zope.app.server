fd475dedff241d385a9e46c9b5e43b149c74b307
Marius Gedminas <marius@gedmin.as>
2017-10-21 17:42:44 +0300

    Whack setup.py with a PEP-8 hammer

--- setup.py
+++ setup.py
@@ -21,53 +21,62 @@
 import os
 from setuptools import setup, find_packages
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-setup(name='zope.app.server',
-      version='3.6.1dev',
-      author='Zope Corporation and Contributors',
-      author_email='zope-dev@zope.org',
-      description='ZServer integration for Zope 3 Applications',
-      long_description=(
-          read('README.txt')
-          + '\n\n' +
-          read('CHANGES.txt')
-          ),
-      keywords = "zope3 zserver server http ftp wsgi",
-      classifiers = [
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/zope.app.server',
-      license='ZPL 2.1',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['zope', 'zope.app'],
-      extras_require = dict(test=['zope.app.testing']),
-      install_requires=['setuptools',
-                        'zope.app.applicationcontrol',
-                        'zope.app.appsetup >= 3.11',
-                        'zope.app.publication',
-                        'zope.app.wsgi',
-                        'zope.configuration',
-                        'zope.deprecation',
-                        'zope.event',
-                        'zope.interface',
-                        'zope.publisher',
-                        'zope.server',
-                        'zope.password >= 3.6',
-                        'zope.processlifetime',
-                        'zdaemon',
-                        'ZConfig',
-                        'ZODB3',
-                        ],
-      include_package_data = True,
-      zip_safe = False,
-      )
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+setup(
+    name='zope.app.server',
+    version='3.6.1dev',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='ZServer integration for Zope 3 Applications',
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+    ),
+    keywords="zope3 zserver server http ftp wsgi",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://cheeseshop.python.org/pypi/zope.app.server',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope', 'zope.app'],
+    extras_require=dict(
+        test=[
+            'zope.app.testing',
+        ],
+    ),
+    install_requires=[
+        'setuptools',
+        'zope.app.applicationcontrol',
+        'zope.app.appsetup >= 3.11',
+        'zope.app.publication',
+        'zope.app.wsgi',
+        'zope.configuration',
+        'zope.deprecation',
+        'zope.event',
+        'zope.interface',
+        'zope.publisher',
+        'zope.server',
+        'zope.password >= 3.6',
+        'zope.processlifetime',
+        'zdaemon',
+        'ZConfig',
+        'ZODB3',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
