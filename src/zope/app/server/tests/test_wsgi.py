5503c0839cb10da0593e6ab530917963d97f8e48
Marius Gedminas <marius@gedmin.as>
2017-10-30 18:43:30 +0200

    100% test coverage for wsgi.py

--- src/zope/app/server/tests/test_wsgi.py
+++ src/zope/app/server/tests/test_wsgi.py
@@ -3,6 +3,7 @@ import unittest
 from zope.interface.verify import verifyObject
 from zope.app.server.servertype import IServerType
 from zope.app.server.wsgi import http, pmhttp
+from zope.server.http.wsgihttpserver import WSGIHTTPServer
 
 
 class TestWSGIServerType(unittest.TestCase):
@@ -10,3 +11,9 @@ class TestWSGIServerType(unittest.TestCase):
     def test(self):
         verifyObject(IServerType, http)
         verifyObject(IServerType, pmhttp)
+
+    def test_create(self):
+        dispatcher = 'my task dispatcher'
+        db = 'my database'
+        server = http.create("HTTP", dispatcher, db)
+        self.assertIsInstance(server, WSGIHTTPServer)
